3d8067d6f080eaaecf46211aa1c5f3ea853e7879
Brandon <brandonjamiller@hotmail.com>
2019-09-24 21:35:06 +0000

    spinner2.js completed, pair programming with Anne Franklin

---
+++ spinner2.js
@@ -0,0 +1,7 @@
+const spin = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ','\r/   ', '\r-  ', '\r|   '];
+let delay = 100;
+for (let i = 0; i < spin.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(spin[i]);
+  }, delay += 200);
+}
